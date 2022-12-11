a8c91de9553955e19a64c96fc3e5172ce66e38fa
unknown <you@example.com>
2022-12-11 12:05:31 +0600

    navber added

---
+++ src/Pages/Navbar/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    const navItems = <>
+        <li className='font-semibold'><Link to='/'>Home</Link></li>
+        <li className='font-semibold'><Link to='/'>Projects</Link></li>
+        <li className='font-semibold'><Link to='/'>About Me</Link></li>
+        <li className='font-semibold'><Link to='/'>Skills</Link></li>
+        <li className='font-semibold'><Link to='/'>Contact</Link></li>
+
+    </>
+    return (
+        <div className="navbar rounded-lg mt-4 bg-stone-800">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                       {navItems}
+                    </ul>
+                </div>
+                <Link className="btn btn-ghost normal-case text-emerald-500 text-xl">Sharif Mirazul Islam</Link>
+            </div>
+            <div className="navbar-end text-white hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                  {navItems} 
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
